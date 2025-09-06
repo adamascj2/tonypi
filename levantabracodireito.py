2f3627c3522d4cd63fae9d23df240b424046bcdc
adamascj2 <adamascj@gmail.com>
2025-09-06 20:20:46 -0300

    Add files via upload

--- levantabracodireito.py
+++ levantabracodireito.py
@@ -12,17 +12,17 @@ try:
     time.sleep(2)
 
     # Move o servo do braço direito para cima
-     AGC.runActionGroup('levantabraco')
+    AGC.runActionGroup('levantabraco')
     time.sleep(2)  # Mantém o braço na posição por 2 segundos
 
     # Retorna o braço para a posição inicial
-     AGC.runActionGroup('stand')
+    AGC.runActionGroup('stand')
 
     time.sleep(1)
 
     print("Programa concluído.")
 
- except KeyboardInterrupt:    # Ctrl c para interromper
+except KeyboardInterrupt:    # Ctrl c para interromper
 	 
 	 
     AGC.runActionGroup ('stand')
